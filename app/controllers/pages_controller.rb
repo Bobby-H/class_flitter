a5e980b4f9c9dd00aa5e1fed59917ee28754e598
crashtestzombie0@gmail.com <crashtestzombie0@gmail.com>
2016-08-05 11:32:18 -0400

    pseudo working order; posts not saving, view messed up

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,12 +3,21 @@ class PagesController < ApplicationController
   end
 
   def home
+    @posts = Post.all
   end
 
   def profile
-    @username = params[:id]
+    @post = Post.new
+    if (User.find_by_username(params[:id]))
+      @username = params[:id]
+    else
+      redirect_to root_path, :notice => "User Not Found!"
+    end
+    @posts = Post.all.where("user_id = ?", User.find_by_username(params[:id]).id)
+    @newPost = Post.new
   end
 
   def explore
+    @posts = Post.all
   end
 end
